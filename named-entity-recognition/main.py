36ec882fc8e0d9b982a99e62c2dd59b899fdedd7
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2017-12-18 17:17:25 +0530

    named entity recognition with nltk

--- named-entity-recognition/main.py
+++ named-entity-recognition/main.py
@@ -1,17 +1,13 @@
-import time
 import nltk
 
-sentence = 'Elon Musk is sending his Tesla Roadster to an orbit around Mars'
-nltk.download()
+sentence = 'Elon and Hawking met at SpaceX last Tuesday to discuss Artificial Intelligence'
+
 try:
     tokenized_sentence = nltk.word_tokenize(sentence)
     tagged_sentence = nltk.pos_tag(tokenized_sentence)
-
     named_entity = nltk.ne_chunk(tagged_sentence)
+    print(named_entity)
     named_entity.draw()
-
-    time.sleep(1)
-
 except Exception as e:
     print(e)
 
